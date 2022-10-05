068e0681da84e8106695a2f280e06038193453cf
TranNgocQuy123 <quyxe1998@gmail.com>
2022-10-05 20:13:14 +0700

    first commit

---
+++ backend/src/lambda/http/createTodo.ts
@@ -0,0 +1,31 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { createTodo } from '../../helpers/todos'
+import { getUserId } from '../utils'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    let userId = getUserId(event)
+    const { todoId, name, dueDate, createdAt, done, attachmentUrl } = await createTodo(userId, newTodo)
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        item: { todoId, name, dueDate, createdAt, done, attachmentUrl }
+      })
+    };
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
